17c4548c5c0d8dff8386ceaca0fe99d0dac73fee
thomasLS64 <thomas.lesciellour@gmail.com>
2015-02-01 20:06:06 +0100

    mod gestionBD.js

--- Serveurs/gestionBD.js
+++ Serveurs/gestionBD.js
@@ -58,7 +58,7 @@ var arretLigneSchema = new mongoose.Schema({
 	ligneId : Schema.ObjectId
 });
 
-// Schéma des véhicule
+//	Schéma des véhicules
 var vehiculeSchema = new mongoose.Schema({
 	longitude : Number,
 	latitude : Number,
@@ -67,7 +67,7 @@ var vehiculeSchema = new mongoose.Schema({
 	date : { type : Date, default : Date.now },
 });
 
-// Création des modèles associés
+// Création des modèles associés aux schémas
 var compagnieModel = mongoose.model('compagnie', compagnieSchema);
 var ligneModel = mongoose.model('ligne', ligneSchema);
 var arretModel = mongoose.model('arret', arretSchema);
@@ -87,14 +87,14 @@ io.on('connection', function(socket) {
 	**	Création d'une compagnie
 	**
 	**		socket.emit('createAgency', {
-	**			email : "Agency email",
-	**			password : "Agency password",
-	**			agency_id : "..",
-	**			agency_name : "..",
-	**			agency_url : "..",
-	**			agency_timezone : "..",
-	**			agency_phone : "..",
-	**			agency_lang : ".."
+	**			email : "...",
+	**			password : "...",
+	**			agency_id : "...",
+	**			agency_name : "...",
+	**			agency_url : "...",
+	**			agency_timezone : "...",
+	**			agency_phone : "...",
+	**			agency_lang : "..."
 	**		});
 	*/
 	socket.on('createAgency', function(d) {
@@ -124,30 +124,30 @@ io.on('connection', function(socket) {
 	**	Mise à jour d'une compagnie
 	**
 	**		query = {
-	**			email : "Agency email",
-	**			password : "Agency password",
-	**			agency_id : "..",
-	**			agency_name : "..",
-	**			agency_url : "..",
-	**			agency_timezone : "..",
-	**			agency_phone : "..",
-	**			agency_lang : ".."
+	**			email : "...",
+	**			password : "...",
+	**			agency_id : "...",
+	**			agency_name : "...",
+	**			agency_url : "...",
+	**			agency_timezone : "...",
+	**			agency_phone : "...",
+	**			agency_lang : "..."
 	**		}
 	**		update = {
-	**			email : "Agency email",
-	**			password : "Agency password",
-	**			agency_id : "..",
-	**			agency_name : "..",
-	**			agency_url : "..",
-	**			agency_timezone : "..",
-	**			agency_phone : "..",
-	**			agency_lang : ".."
+	**			email : "...",
+	**			password : "...",
+	**			agency_id : "...",
+	**			agency_name : "...",
+	**			agency_url : "...",
+	**			agency_timezone : "...",
+	**			agency_phone : "...",
+	**			agency_lang : "..."
 	**		}
 	**
 	**		socket.emit('updateAgency', query, update);
 	*/
 	socket.on('updateAgency', function(query, update) {
-		compagnieModel.findOneAndUpdate(query, update, function(err, compagnie) {
+		compagnieModel.findOneAndUpdate(query, update, function(err, d) {
 			if(err)
 				console.log('Database Agency update : false');
 			else
@@ -159,26 +159,395 @@ io.on('connection', function(socket) {
 	**	Récupération des compagnies
 	**
 	**		query = {
-	**			email : "Agency email",
-	**			password : "Agency password",
-	**			agency_id : "..",
-	**			agency_name : "..",
-	**			agency_url : "..",
-	**			agency_timezone : "..",
-	**			agency_phone : "..",
-	**			agency_lang : ".."
+	**			email : "...",
+	**			password : "...",
+	**			agency_id : "...",
+	**			agency_name : "...",
+	**			agency_url : "...",
+	**			agency_timezone : "...",
+	**			agency_phone : "...",
+	**			agency_lang : "..."
 	**		}
 	**
-	**		socket.emit('updateAgencies', query, callback);
+	**		socket.emit('selectAgencies', query, callback);
 	*/
 	socket.on('selectAgencies', function(query, callback) {
 		compagnieModel.find(query, function(err, d) {
 			if(err)
-				console.log('Database Agency select : false');
+				console.log('Database Agencies select : false');
 			else
-				console.log('Database Agency select : true');
+				console.log('Database Agencies select : true');
 			callback(err, d);
 		});
 	});
 
+
+	/*
+	**	Ligne
+	**	----------------------
+	**
+	**	Création d'une ligne
+	**
+	**		socket.emit('createLine', {
+	**			route_id : "...",
+	**			route_short_name : "...",
+	**			route_long_name : "...",
+	**			route_desc : "...",
+	**			route_type : "...",
+	**			service_id : "...",
+	**			trip_id : "...",
+	**			trip_headsign : "...",
+	**			compagnieId : "..."
+	**		});
+	*/
+	socket.on('createAgency', function(d) {
+		// Initialisation de la nouvelle ligne
+		var newLine = new ligneModel();
+
+		// Peuplement des différents champs
+		newLine.route_id = d.route_id;
+		newLine.route_short_name = d.route_short_name;
+		newLine.route_long_name = d.route_long_name;
+		newLine.route_desc = d.route_desc;
+		newLine.route_type = d.route_type;
+		newLine.service_id = d.service_id;
+		newLine.trip_id = d.trip_id;
+		newLine.trip_headsign = d.trip_headsign;
+		newLine.compagnieId = d.compagnieId;
+
+		// Insertion en base de données
+		newLine.save(function(err) {
+			if(err)
+				console.log('Database Line creation : false');
+			else
+				console.log('Database Line creation : true');
+		});
+	});
+
+	/*
+	**	Mise à jour d'une ligne
+	**
+	**		query = {
+	**			route_id : "...",
+	**			route_short_name : "...",
+	**			route_long_name : "...",
+	**			route_desc : "...",
+	**			route_type : "...",
+	**			service_id : "...",
+	**			trip_id : "...",
+	**			trip_headsign : "...",
+	**			compagnieId : "..."
+	**		}
+	**		update = {
+	**			route_id : "...",
+	**			route_short_name : "...",
+	**			route_long_name : "...",
+	**			route_desc : "...",
+	**			route_type : "...",
+	**			service_id : "...",
+	**			trip_id : "...",
+	**			trip_headsign : "...",
+	**			compagnieId : "..."
+	**		}
+	**
+	**		socket.emit('updateLine', query, update);
+	*/
+	socket.on('updateLine', function(query, update) {
+		ligneModel.findOneAndUpdate(query, update, function(err, d) {
+			if(err)
+				console.log('Database Line update : false');
+			else
+				console.log('Database Line update : true');
+		});
+	});
+
+	/*
+	**	Récupération des lignes
+	**
+	**		query = {
+	**			route_id : "...",
+	**			route_short_name : "...",
+	**			route_long_name : "...",
+	**			route_desc : "...",
+	**			route_type : "...",
+	**			service_id : "...",
+	**			trip_id : "...",
+	**			trip_headsign : "...",
+	**			compagnieId : "..."
+	**		}
+	**
+	**		socket.emit('selectLines', query, callback);
+	*/
+	socket.on('selectLines', function(query, callback) {
+		ligneModel.find(query, function(err, d) {
+			if(err)
+				console.log('Database Lines select : false');
+			else
+				console.log('Database Lines select : true');
+			callback(err, d);
+		});
+	});
+
+
+	/*
+	**	Arret
+	**	----------------------
+	**
+	**	Création d'un arret
+	**
+	**		socket.emit('createStop', {
+	**			stop_id : "...",
+	**			stop_name : "...",
+	**			stop_desc : "...",
+	**			stop_lat : "...",
+	**			stop_lon : "...",
+	**			stop_url : "...",
+	**			location_type : "...",
+	**			compagnieId : "..."
+	**		});
+	*/
+	socket.on('createStop', function(d) {
+		// Initialisation du nouvel arret
+		var newStop = new arretModel();
+
+		// Peuplement des différents champs
+		newStop.stop_id = d.stop_id;
+		newStop.stop_name = d.stop_name;
+		newStop.stop_desc = d.stop_desc;
+		newStop.stop_lat = d.stop_lat;
+		newStop.stop_lon = d.stop_lon;
+		newStop.stop_url = d.stop_url;
+		newStop.location_type = d.location_type;
+		newStop.compagnieId = d.compagnieId;
+
+		// Insertion en base de données
+		newStop.save(function(err) {
+			if(err)
+				console.log('Database Stop creation : false');
+			else
+				console.log('Database Stop creation : true');
+		});
+	});
+
+	/*
+	**	Mise à jour d'un arret
+	**
+	**		query = {
+	**			stop_id : "...",
+	**			stop_name : "...",
+	**			stop_desc : "...",
+	**			stop_lat : "...",
+	**			stop_lon : "...",
+	**			stop_url : "...",
+	**			location_type : "...",
+	**			compagnieId : "..."
+	**		}
+	**		update = {
+	**			stop_id : "...",
+	**			stop_name : "...",
+	**			stop_desc : "...",
+	**			stop_lat : "...",
+	**			stop_lon : "...",
+	**			stop_url : "...",
+	**			location_type : "...",
+	**			compagnieId : "..."
+	**		}
+	**
+	**		socket.emit('updateStop', query, update);
+	*/
+	socket.on('updateStop', function(query, update) {
+		arretModel.findOneAndUpdate(query, update, function(err, d) {
+			if(err)
+				console.log('Database Stop update : false');
+			else
+				console.log('Database Stop update : true');
+		});
+	});
+
+	/*
+	**	Récupération des arrets
+	**
+	**		query = {
+	**			stop_id : "...",
+	**			stop_name : "...",
+	**			stop_desc : "...",
+	**			stop_lat : "...",
+	**			stop_lon : "...",
+	**			stop_url : "...",
+	**			location_type : "...",
+	**			compagnieId : "..."
+	**		}
+	**
+	**		socket.emit('selectStops', query, callback);
+	*/
+	socket.on('selectStops', function(query, callback) {
+		arretModel.find(query, function(err, d) {
+			if(err)
+				console.log('Database Stop select : false');
+			else
+				console.log('Database Stop select : true');
+			callback(err, d);
+		});
+	});
+
+
+	/*
+	**	Arret et Ligne
+	**	----------------------
+	**
+	**	Création d'un lien entre arret et ligne
+	**
+	**		socket.emit('createStopLine', {
+	**			arretId : "...",
+	**			ligneId : "..."
+	**		});
+	*/
+	socket.on('createStopLine', function(d) {
+		// Initialisation du nouveau lien
+		var newStopLine = new arretLigneModel();
+
+		// Peuplement des différents champs
+		newStop.arretId = d.arretId;
+		newStop.ligneId = d.ligneId;
+
+		// Insertion en base de données
+		newStop.save(function(err) {
+			if(err)
+				console.log('Database link between Stop & Line creation : false');
+			else
+				console.log('Database link between Stop & Line creation : true');
+		});
+	});
+
+	/*
+	**	Mise à jour d'un lien entre arret et ligne
+	**
+	**		query = {
+	**			arretId : "...",
+	**			ligneId : "..."
+	**		}
+	**		update = {
+	**			arretId : "...",
+	**			ligneId : "..."
+	**		}
+	**
+	**		socket.emit('updateStopLine', query, update);
+	*/
+	socket.on('updateStopLine', function(query, update) {
+		arretLigneModel.findOneAndUpdate(query, update, function(err, d) {
+			if(err)
+				console.log('Database link between Stop & Line update : false');
+			else
+				console.log('Database link between Stop & Line update : true');
+		});
+	});
+
+	/*
+	**	Récupération des lien entre arrets et lignes
+	**
+	**		query = {
+	**			arretId : "...",
+	**			ligneId : "..."
+	**		}
+	**
+	**		socket.emit('selectStopsLines', query, callback);
+	*/
+	socket.on('selectStopsLines', function(query, callback) {
+		arretLigneModel.find(query, function(err, d) {
+			if(err)
+				console.log('Database link between Stop & Line select : false');
+			else
+				console.log('Database link between Stop & Line select : true');
+			callback(err, d);
+		});
+	});
+
+
+	/*
+	**	Véhicule
+	**	----------------------
+var vehiculeSchema = new mongoose.Schema({
+	longitude : Number,
+	latitude : Number,
+	ligneId : Schema.ObjectId,
+	compagnieId : Schema.ObjectId,
+	date : { type : Date, default : Date.now },
+});
+	**
+	**	Création d'un véhicule
+	**
+	**		socket.emit('createVehicle', {
+	**			longitude = "...",
+	**			latitude = "...",
+	**			ligneId = "...",
+	**			compagnieId = "..."
+	**		});
+	*/
+	socket.on('createVehicle', function(d) {
+		// Initialisation du nouveau véhicule
+		var newVehicle = new vehiculeModel();
+
+		// Peuplement des différents champs
+		newVehicle.longitude = d.longitude;
+		newVehicle.latitude = d.latitude;
+		newVehicle.ligneId = d.ligneId;
+		newVehicle.compagnieId = d.compagnieId;
+
+		// Insertion en base de données
+		newVehicle.save(function(err) {
+			if(err)
+				console.log('Database Vehicle creation : false');
+			else
+				console.log('Database Vehicle creation : true');
+		});
+	});
+
+	/*
+	**	Mise à jour d'un véhicule
+	**
+	**		query = {
+	**			longitude = "...",
+	**			latitude = "...",
+	**			ligneId = "...",
+	**			compagnieId = "..."
+	**		}
+	**		update = {
+	**			longitude = "...",
+	**			latitude = "...",
+	**			ligneId = "...",
+	**			compagnieId = "..."
+	**		}
+	**
+	**		socket.emit('updateVehicle', query, update);
+	*/
+	socket.on('updateVehicle', function(query, update) {
+		vehiculeModel.findOneAndUpdate(query, update, function(err, d) {
+			if(err)
+				console.log('Database Vehicle update : false');
+			else
+				console.log('Database Vehicle update : true');
+		});
+	});
+
+	/*
+	**	Récupération des véhicules
+	**
+	**		query = {
+	**			longitude = "...",
+	**			latitude = "...",
+	**			ligneId = "...",
+	**			compagnieId = "..."
+	**		}
+	**
+	**		socket.emit('selectVehicles', query, callback);
+	*/
+	socket.on('selectVehicles', function(query, callback) {
+		vehiculeModel.find(query, function(err, d) {
+			if(err)
+				console.log('Database Vehicle select : false');
+			else
+				console.log('Database Vehicle select : true');
+			callback(err, d);
+		});
+	});
 });
