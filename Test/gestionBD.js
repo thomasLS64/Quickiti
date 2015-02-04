26ab680a53c68da50ce845efb4a6180f7c18c961
Thomas Le Sciellour <thomas.lesciellour@gmail.com>
2015-02-04 19:05:42 +0100

    Update gestionBD.js test

--- Test/gestionBD.js
+++ Test/gestionBD.js
@@ -1,6 +1,5 @@
 var serveurGestionBD = require('socket.io-client')('http://localhost:7007');
 
-/*
 // Création d'une nouvelle compagnie
 serveurGestionBD.emit('createAgency', {
 	email : "test@test.com",
@@ -10,23 +9,33 @@ serveurGestionBD.emit('createAgency', {
 	agency_timezone : "Europe/Paris",
 	agency_lang : "fr"
 });
-*/
 
-/*
+// Et d'une autre...
+serveurGestionBD.emit('createAgency', {
+	email : "contact@agency.com",
+	password : "BestPassW0rdEver",
+	agency_id : "RTTP",
+	agency_name : "Relational Tribute of Text Postal",
+	agency_timezone : "America/Denver",
+	agency_lang : "fr"
+});
+
+
+
 // Mise à jour d'une compagnie
 serveurGestionBD.emit('updateAgency',{
-	"email" : "beurk2@toto.com"
+	"email" : "test@test.com"
 },
 {
-	"email" : "test@test.com"
+	"email" : "toto@tata.fr",
+	"agency_name" : "Compagnie de toto"
 });
-*/
 
-/*
+
 // Récupération des informations d'une compagnie
 serveurGestionBD.emit('selectAgency', {
-	"email" : "test@test.com"
+	"agency_lang" : "fr"
 }, function(err, d) {
+	if(err) throw err;
 	console.log(d);
 });
-*/
