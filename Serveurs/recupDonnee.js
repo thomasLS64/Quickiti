97be1fe551948744bbea86edcc360a0598ae19a7
BenjaminB64 <benjamin.bordel.64@gmail.com>
2015-02-07 16:52:35 +0100

    Update recupDonnee.js

--- Serveurs/recupDonnee.js
+++ Serveurs/recupDonnee.js
@@ -3,12 +3,12 @@ port = 9009, // Port d'écoute des sockets
 io = require('socket.io')(port), // Socket.IO pour la communication entre serveurs
 _ = require('underscore'),
 color = require('colors'),
-clientGestBD = require('socket.io-client')('http://192.168.1.56:7007/', {'reconnection' : true, 'reconnectionDelay' : 2000, reconnectionDelayMax : 100000 });
+clientGestBD = require('socket.io-client')('http://192.168.0.25:7007/', {'reconnection' : true, 'reconnectionDelay' : 2000, reconnectionDelayMax : 100000 });
 
 clientGestBD.on('connect', function(){ console.info("Connecté au serveur de la base de donnée.".green); clientGestBD.connecte = true; });
 clientGestBD.on('event', function(data){ console.log(data); });
 clientGestBD.on('disconnect', function(){ console.error("Déconnecté du serveur de la base de donnée.".red); clientGestBD.connecte = false; });
-clientGestBD.on('reconnecting', function(n) { console.info("Reconnexion en cours (" + n + ") ...".orange);});
+clientGestBD.on('reconnecting', function(n) { console.info("Reconnexion en cours (".yellow + n + ") ...".yellow); });
 clientGestBD.on('reconnect_error', function() { console.error("Reconnexion échouée".red);});
 clientGestBD.on('reconnect_failed', function() { console.error("Reconnexion impossible.".red); });
 
@@ -31,21 +31,40 @@ var GTFSFiles = [	'agency'
 
 io.on('connection', function (socket) {
 	console.log(colors.cyan("Un client est connecté" + socket.client));
-    socket.on('subscribeAgencies', function (Agencie) {
+	/*
+
+		Agencie {
+			name: "Nom",
+			urlGTFS: "GTFS",
+			email: "",
+			password: "...",
+			adress: "",
+			zipCode: 64000,
+			urlSIRI: ""
+
+		}
+	*/
+    socket.on('agencySubscribe', function (Agency) {
     	console.log("------------------------------".bold);
-    	console.log("Demande d'inscription de l'agence" + Agencie.name);
+    	console.log("Demande d'inscription de l'agence : ".blue + Agencie.name.grey);
+    	console.log("Url GTFS : ".blue + Agencie.urlGTFS.grey);
+    	console.log("Url Siri : ".blue + Agencie.urlSIRI.grey);
+    	console.log("Email : ".blue + Agencie.email.grey);
+    	console.log("Url GTFS : ".blue + Agencie.adress.grey);
     	console.log("------------------------------".bold);
+    	downloadGTFS(Agency.name, Agency.urlGTFS);
     });
     socket.on("requestPerimeter", function (point, perimeter) {
     	console.log("------------------------------".bold);
     	console.log("Demande de périmetre reçue.".purple)
     	if (!clientGestBD.connecte) {
-    		socket.emmit("requestPerimeter", { error: 1 });
+    		socket.emit("requestPerimeter", { error: 1 });
     		console.log("On est déconnecté du serveur de gestion de la bdd.".red);
     	}
     	else {
     		console.log("Connexion à la bdd OK".green);
-    		clientGestBD.emmit('searchStop', { point: point, perimeter: perimeter }, function (result) { 
+    		console.log("Requête à la bdd...");
+    		clientGestBD.emit('searchStop', { point: point, perimeter: perimeter }, function (result) { 
     			if (!result.etat) {
     				console.error("Le serveur de gestion de bd a renvoyé une erreur.".red);
     			}
@@ -63,15 +82,16 @@ io.on('connection', function (socket) {
 			    		});
     				}
     			}
+    			console.log("------------------------------".bold);
+
     		});
     	}
-    	console.log("------------------------------".bold);
     });
 
 });
 
 
-function downloadGTFS(url) {
+function downloadGTFS(name, url) {
 	log('Starting ' + url);
 	async.series([
 			cleanupFiles,
@@ -100,4 +120,34 @@ function cleanupFiles(cb) {
 			}
 		}
 	});
+}
+function downloadFiles(cb) {
+	//do download
+	// update implementation to allow download from local filesystem, to allow testable code
+	//On parse l'URL et on vérifie que le protocole est bien http ou https, sinon on essaye de récuperer le fichier
+	//sur le système local
+	var file_protocol = require('url').parse(agency_url)['protocol']; 
+	if (file_protocol === 'http:' || file_protocol === 'https:') {
+		request(agency_url, processFile).pipe(fs.createWriteStream(downloadDir + '/latest.zip'));
+
+		function processFile(e, response, body){
+			if(response && response.statusCode != 200){ cb(new Error('Couldn\'t download function() {};iles')); }
+			log(agency_key + ': Download successful');
+
+			fs.createReadStream(downloadDir + '/latest.zip')
+			.pipe(unzip.Extract({ path: downloadDir }).on('close', cb))
+			.on('error', handleError);
+		}
+	} else {
+		if (!fs.existsSync(agency_url)) return cb(new Error('File does not exists'));
+		fs.createReadStream(agency_url)
+			.pipe(fs.createWriteStream(downloadDir + '/latest.zip'))
+			.on('close', function(){
+					fs.createReadStream(downloadDir + '/latest.zip')
+					.pipe(unzip.Extract({ path: downloadDir }).on('close', cb))
+					.on('error', handleError);
+				}
+			)
+			.on('error', handleError);
+	}
 }
