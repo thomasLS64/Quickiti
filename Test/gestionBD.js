8e47156d9115254362fdb897d9a94faaabdd2484
thomasLS64 <thomas.lesciellour@gmail.com>
2015-02-07 14:25:06 +0100

    Update gestionBD test

--- Test/gestionBD.js
+++ Test/gestionBD.js
@@ -11,37 +11,47 @@ serveurGestionBD.emit('createAgency', {
 	agency_lang : "fr"
 }, function(etat) {
 
-	// Et d'une autre...
-	console.log('Création de la deuxième compagnie');
-	serveurGestionBD.emit('createAgency', {
-		email : "contact@agency.com",
-		password : "BestPassW0rdEver",
-		agency_id : "RTTP",
-		agency_name : "Relational Tribute of Text Postal",
-		agency_timezone : "America/Denver",
-		agency_lang : "fr"
-	}, function(etat) {
-
-		// Mise à jour d'une compagnie
-		console.log('Mise à jour de la première compagnie');
-		serveurGestionBD.emit('updateAgency', {
-			"email" : "test@test.com"
-		},
-		{
-			"email" : "toto@tata.fr",
-			"agency_name" : "Compagnie de toto"
+	if(!etat)
+		console.log('[ERREUR] Impossible de créer la première compagnie');
+	else {
+		// Et d'une autre...
+		console.log('Création de la deuxième compagnie');
+		serveurGestionBD.emit('createAgency', {
+			email : "contact@agency.com",
+			password : "BestPassW0rdEver",
+			agency_id : "RTTP",
+			agency_name : "Relational Tribute of Text Postal",
+			agency_timezone : "America/Denver",
+			agency_lang : "fr"
 		}, function(etat) {
 
-			// Récupération des informations d'une compagnie
-			console.log('Sélection des compagnies');
-			serveurGestionBD.emit('selectAgencies', {
-				"agency_lang" : "fr"
-			}, function(err, d) {
-				console.log(d);
-			});
+			if(!etat)
+				console.log('[ERREUR] Impossible de créer la deuxième compagnie');
+			else {
+				// Mise à jour d'une compagnie
+				console.log('Mise à jour de la première compagnie');
+				serveurGestionBD.emit('updateAgency', {
+					"email" : "test@test.com"
+				},
+				{
+					"email" : "toto@tata.fr",
+					"agency_name" : "Compagnie de toto"
+				}, function(etat) {
 
+					if(!etat)
+						console.log('[ERREUR] Impossible de mettre à jour la première compagnie');
+					else {
+						// Récupération des informations d'une compagnie
+						console.log('Sélection des compagnies');
+						serveurGestionBD.emit('selectAgencies', {
+							"agency_lang" : "fr"
+						}, function(err, d) {
+							console.log(d);
+						});
+					}
+				});
+			}
 		});
-
-	});
+	}
 
 });
