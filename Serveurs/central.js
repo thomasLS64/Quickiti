fd850ab87c2add1183585c41a0b1ad7c8a273176
Adrian <adrian.chauvel@gmail.com>
2015-02-07 20:05:28 +0100

    Update stopsNearTo central.js

--- Serveurs/central.js
+++ Serveurs/central.js
@@ -15,7 +15,7 @@ Car c'est au client de se connecter
 
 
 // Quand un client se connecte en appellant la fonction con, ici le serveur site client et pour le site pour les compagnies 
-io.sockets.on('connection', function (socket, callback) { // socket pour avoir une instance différente..utile?
+io.sockets.on('connection', function (socket, callback) { // socket pour avoir une instance différente pour chaque seveur
     /* lorsque la fonction "clientRequest" est appelé depuis le site client
     ** 
     ** description des paramètres de la fonction:
@@ -43,9 +43,8 @@ io.sockets.on('connection', function (socket, callback) { // socket pour avoir u
                                                  perimeter = perimeter},
                                                 function (etat, routes){
                 if(etat){
+
                     /*
-                    **
-                    **
                     ** description des paramètres de la fonction:
                     ** serveurRecuperationDonnees.emit('searchRealTime',
                     **                                 {routes : routes},
@@ -59,7 +58,6 @@ io.sockets.on('connection', function (socket, callback) { // socket pour avoir u
                     ** var line = { time1 , time2, .. , timeN} // avec (timeN-time1 < 5) heures par exemple
                     **
                     */
-
                     serveurRecuperationDonnees.emit('searchRoutesRealTime',
                                                     {routes : routes},
                                                     function (etat, routesRealTime){
@@ -77,82 +75,42 @@ io.sockets.on('connection', function (socket, callback) { // socket pour avoir u
             });
         }
         else if(requestType == 'stopsNearTo'){
+            // demande à la base de donnée de calculé les arrêts à proximités et les informations stocké correspondantes
+            serverGestionBD.emit('searchStopsNearTo',
+                                                {request = request,
+                                                 perimeter = perimeter},
+                                                function (etat, stopsNearTo){
+                if(etat){
 
+                    /*
+                    ** description des paramètres de la fonction:
+                    ** serveurRecuperationDonnees.emit('searchStopsNearTo',
+                    **                                 {routes : routes},
+                    **                                 function (etat, stopsNearToRealTime){});
+                    ** 
+                    ** var stopsNearTo = { stop1{..}, stop2{..}, .. , stopN{..} }
+                    ** var stopS = { stopAgency1{..}, stopAgency2{..}, .. , stopAgencyN }
+                    ** var stopStartAgency = { line1{..}, line2{..}, .. , lineN{..} } // agence de l'arrêt de départ
+                    ** var line = { time1 , time2, .. , timeN} // avec (timeN-time1 < 5) heures par exemple
+                    **
+                    */
+                    serveurRecuperationDonnees.emit('searchStopsNearToRealTime',
+                                                    {routes : routes},
+                                                    function (etat, stopsNearToRealTime){
+                        if(etat){
+                            if(callback) callback(etat, stopsNearToRealTime);
+                        }
+                        else{
+                            if(callback) callback(etat, stopsNearTo);
+                        }
+                    });
+                }
+                else {
+                    console.log('Erreur dans la recuperation de l itineraire')
+                }
+            });
         }
         else{
             console.log('La fonctionnalite  ' requestType '  n pas encore ete developpe');
-
-        }
-               
-
-
-
-               /* if(etat){
-                    // Parcours des différentes lignes reçu
-                    for (var i = 0; i < lignes.length; i++) {
-                        ligne = lignes[i];
-                        if (ligne.tempsReel == 'tempsReel') {
-                            reponseFinal += ligne;
-                        } 
-                        else{
-                            serverGestionBD.emit('horaireLigne', ligne, function(etat, horaires) {
-                                if(etat){
-                                    if (horaires.length > 0) {
-                                        reponseFinal += ligne; // + quoi ?
-                                        } 
-                                    else{
-                                        serverGestionBD.emit('compagnieLigne',ligne, function(compagnie){
-                                            if (compagnie) {
-                                                reponseFinal += ligne + caracteristiques; // "caracteristiques" sort d'où ?
-                                            }
-                                        });
-                                    }
-                                }
-                            });
-                        }
-                    }
-                }
-            } 
-        });
-            
-            
-        else{
-            // initialisation variable
-            var ligne;
-
-            // demande la liste des lignes au Serveur de récupération de données
-            var lignes = socketRecuperationDonnees.emit('recuperationArret', demande, perimeter, function(lignes));
-            
-            // Parcours des différentes lignes reçu
-            for (var i = 0; i < lignes.length; i++) {
-                ligne = lignes[i];
-                if (ligne.tempsReel == 'tempsReel') {
-                    reponseFinal += ligne;
-                } 
-                else{
-                    var horaires = serverGestionBD.emit('horaireLigne', ligne, function(horaires){
-                        if (horaires.length > 0) {
-                            reponseFinal += ligne; // + quoi ?
-                        } 
-                        else{
-                            // Récupération des informations d'une compagnie
-                            console.log('Sélection des compagnies');
-                            serverGestionBD.emit('selectAgencies', {
-                                    "agency_lang" : "fr"
-                            }, function(err, compagnie) {
-                                    console.log(compagnie);
-                                    if (compagnie) {
-                                        reponseFinal += ligne + caracteristiques; // "caracteristiques" sort d'où ?
-                                    }
-                            });
-                            
-                        }
-                    });
-                    
-                }
-            }
-        }
-    });*/
-
-    
+        }            
 });
