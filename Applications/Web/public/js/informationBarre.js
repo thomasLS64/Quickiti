899fad65e0674ddda0ccfe7fa6a299346feb00af
thomasLS64 <thomas.lesciellour@gmail.com>
2015-03-05 17:55:12 +0100

    update web app

--- Applications/Web/public/js/informationBarre.js
+++ Applications/Web/public/js/informationBarre.js
@@ -13,8 +13,8 @@ window.onload = function() {
 	// Navigation dans les différentes screens-cards
 	for(var i=0; i<screensCards.length; i++) {
 		var screensCard = screensCards[i],
-			links = screensCard.querySelectorAll('.nav-screens li'),
-			screens = screensCard.querySelectorAll('.screens li');
+			links = screensCard.querySelectorAll('.nav-screens>li'),
+			screens = screensCard.querySelectorAll('.screens>li');
 
 		for(var k=0; k<links.length; k++) {
 			links[k].onclick = function() {
@@ -25,10 +25,10 @@ window.onload = function() {
 				this.setAttribute('class', 'actif');
 
 				// Retirer l'ancienne vue
-				screensCard.querySelector('.screens li.actif').removeAttribute('class');
+				screensCard.querySelector('.screens>li.actif').removeAttribute('class');
 				
 				// Afficher nouvelle vue
-				screensCard.querySelector('.screens li:nth-child('+parseInt(this.getAttribute('screen'))+')').setAttribute('class', 'actif');
+				screensCard.querySelector('.screens>li:nth-child('+parseInt(this.getAttribute('screen'))+')').setAttribute('class', 'actif');
 			}
 		}
 	}
@@ -36,12 +36,9 @@ window.onload = function() {
 
 	// Traitement de la recherche simple
 	formSimpleSearch.onsubmit = function(e) {
-		// Variables
 		var locality = document.querySelector('#locality').value;
 
-		// Annuler comportement par défaut
 		e.preventDefault();
-
 
 		recupInformationsByAdress(locality, function(point) {
 			var center = {lat: 0, lng: 0},
@@ -68,6 +65,10 @@ window.onload = function() {
 			map.fitBounds(markers.getBounds());
 		});
 	}
+
+	formSimpleSearch.onkeyup = function(e) {
+		suggestSearch(e);
+	};
 
 	// Traitement de la recherche d'itinéraire
 	formRoutesSearch.onsubmit = function(e) {
@@ -118,28 +119,67 @@ window.onload = function() {
 		});
 
 	}
-}
 
-var recupInformationsByAdress = function(lieu, callback) {
-	var XMLHttp;
+	formRoutesSearch.onkeyup = function(e) {
+		suggestSearch(e);
+	};
 
-	if(window.XMLHttpRequest)
-		XMLHttp = new XMLHttpRequest();
-	else
-		XMLHttp = new ActiveXObject("Microsoft.XMLHTTP");
+	var recupInformationsByAdress = function(lieu, callback) {
+		var XMLHttp;
 
-	XMLHttp.onreadystatechange = function() {
-		if(XMLHttp.readyState == 4) {
-			if(XMLHttp.status == 200) {
-				var reponse = JSON.parse(XMLHttp.responseText);
+		if(window.XMLHttpRequest)
+			XMLHttp = new XMLHttpRequest();
+		else
+			XMLHttp = new ActiveXObject("Microsoft.XMLHTTP");
 
-				if(callback) callback(reponse);
+		XMLHttp.onreadystatechange = function() {
+			if(XMLHttp.readyState == 4) {
+				if(XMLHttp.status == 200) {
+					var reponse = JSON.parse(XMLHttp.responseText);
+
+					if(callback) callback(reponse);
+				}
 			}
 		}
+
+		XMLHttp.open('GET', 
+			'https://maps.googleapis.com/maps/api/geocode/json?address='+lieu,
+			true);
+		XMLHttp.send();
 	}
 
-	XMLHttp.open('GET', 
-		'https://maps.googleapis.com/maps/api/geocode/json?address='+lieu,
-		true);
-	XMLHttp.send();
+	var suggestSearch = function(event) {
+		var src = event.srcElement,
+			value = src.value,
+			parent = src.parentNode,
+			suggestions = src.nextElementSibling;
+
+		if(suggestions.getAttribute('class') == 'suggestions')
+			suggestions.classList.add('actif')
+
+		for(var i=0; i<suggestions.querySelectorAll('li').length; i++) {
+			suggestions.removeChild(suggestions.querySelectorAll('li')[i]);
+		}
+
+		recupInformationsByAdress(value, function(response) {
+			for(var i in response.results) {
+				var li = document.createElement('li');
+				li.innerHTML = response.results[i].formatted_address;
+				li.onclick = function() {
+					var value = this.innerHTML,
+						parent = this.parentNode,
+						input = parent.previousElementSibling,
+						form = parent.parentNode;
+
+					input.value = value;
+					input.focus();
+					suggestions.classList.remove('actif');
+					//form.submit();
+				};
+
+				suggestions.appendChild(li);
+			}
+		});
+
+	};
 }
